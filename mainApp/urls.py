f98e4a43d5768391dd50ae96e78289be501d0917
shubham <shubhamkumar252083@gmail.com>
2021-12-17 21:24:10 +0530

    url set for home view

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # path("rough/", include("rough.urls")),
+    path("", include("projects.urls")),
     path('admin/', admin.site.urls),
-    path("projects/", include("projects.urls")),
     path("blog/", include("blog.urls")),
     path("contactMe/", include("contactMe.urls")),
 ]
